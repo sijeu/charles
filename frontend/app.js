bb1ba6d3a9343cad4adb3d2fb6604b1f0230aad4
sijeu <charlesjuliannvaldez22@gmail.com>
2026-09-21 23:14:04 +0800

    Change API URL to production endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://charles-8khr.onrender.com/";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
